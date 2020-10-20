f24683e95a82cfa8b52cc3a35efb76be36f82257
ztt <zttongdeyx@126.com>
2020-10-20 16:35:10 +0800

    进行中

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 //可改动页面
 
-//var domain = "http://mubear.cn"; //统一接口域名，测试环境
-var domain = "http://localhost:8886"; //统一接口域名，测试环境
+var domain = "https://www.mubear.cn"; //统一接口域名，测试环境
+// var domain = "http://localhost:8886"; //统一接口域名，测试环境
 
 exports.domain = domain;
 
